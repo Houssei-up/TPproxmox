b5a382806356b9050fe39bbf85e3ff956693d642
Houssei-up <63685988+Houssei-up@users.noreply.github.com>
2022-03-31 23:15:46 +0200

    Update Create_container.sh

--- Create_container.sh
+++ Create_container.sh
@@ -7,4 +7,4 @@ echo $1
 echo $2
 echo $3
 echo "$3" > Filekey
-pct create $CURRENT_ID $IMAGE_ID --rootfs local-lvm:8 --hostname $1 --nameserver 8.8.8.8 --net0 name=eth0,bridge=vmbr0,gw=192.168.56.254,ip=$2 --ssh-public-keys Filekey --start 1
+pct create $CURRENT_ID $IMAGE_ID --rootfs local-lvm:8 --hostname $1 --nameserver 8.8.8.8 -net0 name=eth0,bridge=vmbr0,gw=192.168.56.254,ip=$2 --ssh-public-keys Filekey --start 1
