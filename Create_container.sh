3a664411dd13274a04d1eb19b3cb7e71859863f7
Houssei-up <63685988+Houssei-up@users.noreply.github.com>
2022-03-31 19:40:33 +0200

    Update Create_container.sh

--- Create_container.sh
+++ Create_container.sh
@@ -8,7 +8,3 @@ echo $2
 echo $3
 echo "$3" > Filekey
 pct create $CURRENT_ID $IMAGE_ID --rootfs local-lvm:8 --hostname $1 --nameserver 8.8.8.8 -net0 name=eth0,bridge=vmbr0,gw=192.168.56.254,ip=$2 --ssh-public-keys Filekey
-
-apt update -y
-apt install -y nginx
-systemctl enable nginx
