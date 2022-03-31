455247ab3f6e4b086a6a40e3899618b906bcdcaf
Houssei-up <63685988+Houssei-up@users.noreply.github.com>
2022-03-31 17:05:00 +0200

    Update Create_container.sh

--- Create_container.sh
+++ Create_container.sh
@@ -1,3 +1,8 @@
 IMAGE_ID=$(pveam list local | grep debian | cut -d" " -f1)
 CURRENT_ID=$(((100+$(pct list | wc -l))-1))
+
 pct create $CURRENT_ID $IMAGE_ID --rootfs local-lvm:8 --hostname $Container_name --nameserver 8.8.8.8 -net0 name=eth0,bridge=vmbr0,gw= 192.168.56.254,ip=$Container_ip
+
+apt update -y
+apt install -y nginx
+systemctl enable nginx
