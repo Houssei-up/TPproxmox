0ea81c0667c3276d6998c69c4a4121b8adf9022c
Houssei-up <63685988+Houssei-up@users.noreply.github.com>
2022-03-31 19:04:17 +0200

    Update Create_container.sh

--- Create_container.sh
+++ Create_container.sh
@@ -1,6 +1,10 @@
 IMAGE_ID=$(pveam list local | grep debian | cut -d" " -f1)
 CURRENT_ID=$(((100+$(pct list | wc -l))-1))
 
+echo $IMAGE_ID
+echo $CURRENT_ID
+echo $Container_name
+echo $Container_ip
 pct create $CURRENT_ID $IMAGE_ID --rootfs local-lvm:8 --hostname $Container_name --nameserver 8.8.8.8 -net0 name=eth0,bridge=vmbr0,gw=192.168.56.254,ip=$Container_ip
 
 apt update -y
