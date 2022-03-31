2ef2a20ab7fac86e78ef94d8fc82f2b045799f96
Houssei-up <63685988+Houssei-up@users.noreply.github.com>
2022-03-31 19:12:17 +0200

    Update Create_container.sh

--- Create_container.sh
+++ Create_container.sh
@@ -4,8 +4,8 @@ CURRENT_ID=$(((100+$(pct list | wc -l))-1))
 echo $IMAGE_ID
 echo $CURRENT_ID
 echo $Container_name
-echo $Container_ip
-pct create $CURRENT_ID $IMAGE_ID --rootfs local-lvm:8 --hostname $Container_name --nameserver 8.8.8.8 -net0 name=eth0,bridge=vmbr0,gw=192.168.56.254,ip=$Container_ip
+echo $c_ip
+pct create $CURRENT_ID $IMAGE_ID --rootfs local-lvm:8 --hostname $Container_name --nameserver 8.8.8.8 -net0 name=eth0,bridge=vmbr0,gw=192.168.56.254,ip=$c_ip
 
 apt update -y
 apt install -y nginx
